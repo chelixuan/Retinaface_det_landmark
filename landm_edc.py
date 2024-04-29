6c683c845f261b2cbace6d6caa8761022a6945af
chelixuan <837116734@qq.com>
2024-04-29 10:34:52 +0800

    support focal loss

--- landm_edc.py
+++ landm_edc.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 
 gt_root = '/home/chelx/dataset/M3_dataset/TAG/'
 pred_root = '/home/chelx/ckpt/QC_code/Retinaface/base_mobile0.25/test_txt/'
+save_scatter_path = './pred_offset.png'
 
 folders = ['01', '02', '08']
 
 def eucliDist(A,B):
     return math.sqrt(sum([(a - b)**2 for (a,b) in zip(A,B)]))
 
-mean_edc = []
-max_edc = []
+mean_euc = []
+max_euc = []
 for i in range(len(folders)):
     folder = folders[i]
     print(f'\n{i+1}/{len(folders)} : {folder}')
@@ -43,33 +44,36 @@ for i in range(len(folders)):
         pred_p3_x, pred_p3_y = float(pred_points[4]), float(pred_points[5])
         pred_p4_x, pred_p4_y = float(pred_points[6]), float(pred_points[7])
 
-        edc_dis_1 = eucliDist((gt_p1_x, gt_p1_y), (pred_p1_x, pred_p1_y))
-        edc_dis_2 = eucliDist((gt_p2_x, gt_p2_y), (pred_p2_x, pred_p2_y))
-        edc_dis_3 = eucliDist((gt_p3_x, gt_p3_y), (pred_p3_x, pred_p3_y))
-        edc_dis_4 = eucliDist((gt_p4_x, gt_p4_y), (pred_p4_x, pred_p4_y))
+        euc_dis_1 = eucliDist((gt_p1_x, gt_p1_y), (pred_p1_x, pred_p1_y))
+        euc_dis_2 = eucliDist((gt_p2_x, gt_p2_y), (pred_p2_x, pred_p2_y))
+        euc_dis_3 = eucliDist((gt_p3_x, gt_p3_y), (pred_p3_x, pred_p3_y))
+        euc_dis_4 = eucliDist((gt_p4_x, gt_p4_y), (pred_p4_x, pred_p4_y))
 
-        mean_edc_dis = (edc_dis_1 + edc_dis_2 + edc_dis_3 + edc_dis_4) / 4
-        max_edc_dis = max(edc_dis_1,  edc_dis_2, edc_dis_3, edc_dis_4)
+        mean_euc_dis = (euc_dis_1 + euc_dis_2 + euc_dis_3 + euc_dis_4) / 4
+        max_euc_dis = max(euc_dis_1,  euc_dis_2, euc_dis_3, euc_dis_4)
 
-        # mean_edc.append(mean_edc_dis)
-        # max_edc.append(max_edc_dis)
+        # mean_euc.append(mean_euc_dis)
+        # max_euc.append(max_euc_dis)
 
-        if mean_edc_dis > 100 or max_edc_dis > 100:
+        if mean_euc_dis > 100 or max_euc_dis > 100:
             continue
         else:
-            mean_edc.append(mean_edc_dis)
-            max_edc.append(max_edc_dis)
+            mean_euc.append(mean_euc_dis)
+            max_euc.append(max_euc_dis)
 
-assert len(mean_edc) == len(max_edc), "please check edc collect process"
-x = list(range(0, len(mean_edc)))
+assert len(mean_euc) == len(max_euc), "please check euc collect process"
+x = list(range(0, len(mean_euc)))
 
 fig,axes = plt.subplots(nrows=1, ncols=2)
 ax1 = axes[0]
 ax2 = axes[1]
 
-ax1.scatter(x, mean_edc, s=1)
-ax1.set_ylabel('mean_edc')
-ax2.scatter(x, max_edc, s=1, color='green')
-ax2.set_ylabel('max_edc')
+ax1.scatter(x, mean_euc, s=1)
+ax1.set_ylabel('mean_euc')
+ax2.scatter(x, max_euc, s=1, color='green')
+ax2.set_ylabel('max_euc')
 
-plt.savefig('./pred_offset.png')
+plt.savefig(save_scatter_path)
+
+print('mean_mean-euc = ', round(sum(mean_euc) / len(mean_euc), 3))
+print('mean_max-euc = ', round(sum(max_euc) / len(max_euc), 3))
